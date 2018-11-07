61d3f3a187ce38ba39f2e8631789e99be6c00b8c
eshenko <eshenko@ESHENKO>
2018-11-07 13:25:06 +0200

    Create PrintRecursionNum solushion

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumber(12345);
+            Console.ReadKey();
+        }
+
+        public static void PrintNumber(int num)
+        {
+            if (num > 9)
+            {
+                PrintNumber((int)(num/10));
+            }
+            Console.Write(num % 10);
+        }
+    }
+}
+
+/*
+ * Используя команды Console.Write() лишь при x = 0..9, 
+ * написать рекурсивную программу печати десятичной 
+ * записи целого положительного числа n.
+*/
